6a5de30cacc8c652d43fb5fef893d97f44c17035
joaovitorsramos <joaovitor-soares@hotmail.com>
2020-12-16 12:05:34 -0300

    lambda function updated

--- lambda-orderConsumer/lambdaconsumer1/handler.js
+++ lambda-orderConsumer/lambdaconsumer1/handler.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports.orderCreatedConsumer1 = async event => {
-  console.log('--- Order created in consumer 1! ---')
+  console.log('--- Order created in lambda consumer 1! ---')
   console.log(JSON.stringify(event, null, 2))
 };
 
